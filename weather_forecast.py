dc396a5fca6662d75e29b48e667bea1c78ea7876
= <nkpratt04@gmail.com>
2023-02-22 16:16:04 -0600

    Add main function

--- weather_forecast.py
+++ weather_forecast.py
@@ -6,5 +6,11 @@ key = os.environ.get('WEATHER_KEY')
 weather_forecast_url = f'http://api.openweathermap.org/data/2.5/forecast'
 query = {'q': 'minneapolis', 'units': 'imperial', 'appid': key}
 
+def main():
+    """"""
 
+
+def get_weather_forecast(url, query):
+    """Retrieve API response data and catch errors."""
+    response = requests.get(weather_forecast_url)
     
