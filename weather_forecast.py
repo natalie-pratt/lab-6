43b479463a197c692b007fab377bc8e7d040336f
= <nkpratt04@gmail.com>
2023-02-22 16:24:31 -0600

    Add function to get location from user to be used to find forecast

--- weather_forecast.py
+++ weather_forecast.py
@@ -9,6 +9,20 @@ query = {'q': 'minneapolis', 'units': 'imperial', 'appid': key}
 def main():
     """"""
 
+def get_location():
+    """Get location input from user to determine where
+    forecast should be retrieved from"""
+    city, country = '', ''
+
+    while len(city) == 0:
+        city = input('Enter the name of the city').strip()
+    
+    while len(country) != 2 or not country.isalpha():
+        country = input('Enter the 2-letter country code: ').strip()
+
+    location = f'{city}, {country}'
+    return location
+        
 
 def get_weather_forecast():
     """Retrieve API response data and catch errors."""
