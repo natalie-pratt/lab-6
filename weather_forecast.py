2ed01540966c595ecc61ef5b5720397602fcf4e1
= <nkpratt04@gmail.com>
2023-02-22 16:12:53 -0600

    Cut URL down to base, add query dictionary instead to be added later

--- weather_forecast.py
+++ weather_forecast.py
@@ -3,4 +3,8 @@ import os
 
 key = os.environ.get('WEATHER_KEY')
 
-weather_forecast_url = f'http://api.openweathermap.org/data/2.5/forecast?q=minneapolis,us&units=imperial&appid={key}'
+weather_forecast_url = f'http://api.openweathermap.org/data/2.5/forecast'
+query = {'q': 'minneapolis', 'units': 'imperial', 'appid': key}
+
+
+    
